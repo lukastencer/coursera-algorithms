a980a329fcf4ddd75526bf59147e2389479cfd2a
Lukas Tencer <lukas.tencer@gmail.com>
2014-07-26 19:48:19 -0400

    union find quick union and qeighted with path compression

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-from union_find import UFind
+from union_find import UFind,UFindQUnion,UFindWeightPathComp
+
+print '--------------test quick find---------------------'
 
 UFTestObject = UFind(10)
 
@@ -16,3 +18,50 @@ UFTestObject.union(1, 4)
 print UFTestObject.getRecords()
 UFTestObject.union(0, 8)
 print UFTestObject.getRecords()
+print UFTestObject.components() == 3
+
+print '--------------test quick union---------------------'
+
+UFTestObject = UFindQUnion(10)
+
+UFTestObject.union(0, 1)
+print UFTestObject.getRecords()
+UFTestObject.union(2, 3)
+print UFTestObject.getRecords()
+UFTestObject.union(4, 5)
+print UFTestObject.getRecords()
+UFTestObject.union(6, 7)
+print UFTestObject.getRecords()
+UFTestObject.union(8, 9)
+print UFTestObject.getRecords()
+UFTestObject.union(1, 4)
+print UFTestObject.getRecords()
+UFTestObject.union(0, 8)
+print UFTestObject.getRecords()
+print UFTestObject.components() == 3
+
+print '---------test quick union find weighted--------------'
+
+UFTestObject = UFindWeightPathComp(10)
+
+UFTestObject.union(0, 1)
+print UFTestObject.getRecords()
+UFTestObject.union(2, 3)
+print UFTestObject.getRecords()
+UFTestObject.union(4, 5)
+print UFTestObject.getRecords()
+UFTestObject.union(6, 7)
+print UFTestObject.getRecords()
+UFTestObject.union(8, 9)
+print UFTestObject.getRecords()
+UFTestObject.union(1, 4)
+print UFTestObject.getRecords()
+UFTestObject.union(0, 8)
+print UFTestObject.getRecords()
+print UFTestObject.components() == 3
+print UFTestObject.find(0, 9) == True
+print UFTestObject.find(0, 1) == True
+print UFTestObject.find(0, 2) == False
+print UFTestObject.find(2, 3) == True
+print UFTestObject.find(2, 6) == False
+print UFTestObject.find(0, 9) == True
--- union_find.py
+++ union_find.py
@@ -25,3 +25,85 @@ class UFind():
     
     def getRecords(self):
         return self.__records
+    
+class UFindQUnion():
+    
+    __records = [] 
+    
+    def __init__(self,numElements):
+        
+        self.__records = [0]*numElements
+        for i in range(0,numElements):
+            self.__records[i]=i
+    
+    def root(self,elemIdx):
+        while(self.__records[elemIdx] != elemIdx):
+            elemIdx = self.__records[elemIdx]
+        
+        return elemIdx
+        
+    def find(self,elem1,elem2):
+        return self.root(elem1) == self.root(elem2)
+    
+    def union(self,elem1,elem2):
+        r1 = self.root(elem1)
+        r2 = self.root(elem2)
+        
+        self.__records[r1] = r2
+        
+    def components(self):
+        uniqRoots = set()
+        
+        for i in range(0,len(self.__records)):
+            uniqRoots.add(self.root(i))
+            
+        return len(uniqRoots)
+    
+    def getRecords(self):
+        return self.__records
+    
+class UFindWeightPathComp():
+    
+    __records = [] 
+    __treeSizes = []
+    
+    def __init__(self,numElements):
+        
+        self.__records = [0]*numElements
+        self.__treeSizes = [1]*numElements
+        
+        for i in range(0,numElements):
+            self.__records[i]=i
+    
+    def root(self,elemIdx):
+        while(self.__records[elemIdx] != elemIdx):
+            self.__records[elemIdx] = self.__records[self.__records[elemIdx]] 
+            elemIdx = self.__records[elemIdx]
+        
+        return elemIdx
+        
+    def find(self,elem1,elem2):
+        return self.root(elem1) == self.root(elem2)
+    
+    def union(self,elem1,elem2):
+        r1 = self.root(elem1)
+        r2 = self.root(elem2)
+        
+        if self.__treeSizes[r1] < self.__treeSizes[r2]:
+            self.__records[r1] = r2
+            self.__treeSizes[r2] += self.__treeSizes[r1]
+        else:
+            self.__records[r2] = r1
+            self.__treeSizes[r1] += self.__treeSizes[r2]
+
+        
+    def components(self):
+        uniqRoots = set()
+        
+        for i in range(0,len(self.__records)):
+            uniqRoots.add(self.root(i))
+            
+        return len(uniqRoots)
+    
+    def getRecords(self):
+        return self.__records
