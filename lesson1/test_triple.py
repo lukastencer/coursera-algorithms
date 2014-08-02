8c0b9a334e0f98610940bec34d18a6b62e84e5fa
Lukas Tencer <lukas.tencer@gmail.com>
2014-08-02 17:14:06 -0400

    triple and test_trpile, creating lesson2

---
+++ lesson1/test_triple.py
@@ -0,0 +1,7 @@
+from triple import triple
+
+tri = triple()
+
+array = [5, 10, 20, -30, 1, 2, -7]
+
+print tri.find(array)
---
+++ lesson1/triple.py
@@ -0,0 +1,18 @@
+from binsearch import binsearch
+        
+
+class triple:
+    
+    bs = binsearch()
+    
+    def find(self,inArray):
+        
+        res = set()
+        inArray = sorted(inArray)
+        
+        for i in inArray:
+            for j in inArray:
+                diff = -(i+j)
+                if not self.bs.search(inArray, diff) == -1:
+                    res.add(tuple(sorted([i,j,diff])))
+        return res
