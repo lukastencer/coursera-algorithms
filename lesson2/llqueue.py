5739f2514e8e93a5a6003d78256a89e4162b480d
Lukas Tencer <lukas.tencer@gmail.com>
2014-08-03 10:25:52 -0400

    llqueue and test_llqueue

---
+++ lesson2/llqueue.py
@@ -0,0 +1,48 @@
+class llqueue:
+    
+    class node():
+        
+        next = None
+        val = None
+        
+        def __init__(self,nextNode,val):
+                    
+            self.next = nextNode
+            self.val = val
+        
+    head = None
+    tail = None
+    
+    def push(self,inVal):
+        
+        if self.head == None:
+            self.head = self.node(None,inVal)
+            self.tail = self.head
+            
+        else:
+            tempT = self.tail
+            self.tail = self.node(None,inVal)
+            tempT.next=self.tail
+        
+    def pop(self):
+        item = self.head.val;
+        self.head = self.head.next;
+        return item
+    
+    def show(self):
+        tempP = self.head
+        print '-------'
+        
+        while not tempP == None:
+            print tempP.val
+            tempP = tempP.next
+    
+    def parse(self,inString):
+        
+        parts = inString.split()
+        
+        for part in parts:
+            if part == '-':
+                self.pop()
+            else:
+                self.push(part)
---
+++ lesson2/test_llqueue.py
@@ -0,0 +1,16 @@
+from llqueue import llqueue
+
+llq = llqueue()
+
+llq.show()
+llq.push('wow')
+llq.show()
+llq.push('this')
+llq.show()
+llq.push('is')
+llq.push('some')
+llq.show()
+llq.pop()
+llq.pop()
+llq.push('fish')
+llq.show()
